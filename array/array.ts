e256e9d7f60e6bc42e6106cf9c2ac65bca0709ee
Abid Noman <abidnoman888@gmail.com>
2024-01-08 22:36:16 +0500

    live

--- array/array.ts
+++ array/array.ts
@@ -1,16 +1,45 @@
-const string = ['a', 'b', 'c', 'd']
-// 4*4 = 16 bytes of storage
+class MyArray {
+  length: number;
+  data: Map<number, any> = new Map();
+  constructor(){
+    this.length = 0;
+  }
 
-//push
-string.push('e') // O(1)
+  get(index: number): any{
+    return this.data.get(index)
+  }
 
-//pop
-string.pop();
-string.pop();
+  push(item: any): number{
+    this.data.set(this.length, item);
+    this.length++;
 
-//unshift
-string.unshift('x') // O(n)
+    return this.length;
+  }
 
-string.splice(0, 1, 'dsa') // O(n)
+  pop(): any{
+    let item = this.data.get(this.length - 1);
+    this.data.delete(this.length - 1);
+    this.length--;
+    return item;
+  }
 
-console.log(string);
+  delete(index: number){
+    let item = this.data.get(this.length - 1);
+    this.shift(index);
+  }
+
+  shift(index: number){
+    for (let i = index; i < this.length - 1; i++) {
+      this.data.set(i, this.data.get(i+1));
+      this.data.delete(this.length - 1);
+      this.length--;
+    }
+  }
+}
+
+const newArray = new MyArray();
+newArray.push('HI')
+newArray.push('You')
+newArray.push('!')
+newArray.delete(1)
+console.log(newArray);
